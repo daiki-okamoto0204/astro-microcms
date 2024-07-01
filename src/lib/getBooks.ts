4645734f8e1bc8f98502438b7e40a36991ee7c5f
d.okamoto <daiki6916@gmail.com>
2024-07-01 23:37:21 +0900

    first commit

---
+++ src/lib/getBooks.ts
@@ -0,0 +1,56 @@
+import { createClient } from "microcms-js-sdk";
+import type { MicroCMSDate, MicroCMSQueries } from "microcms-js-sdk";
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+export type Category = {
+	id: string;
+	name: string;
+}& MicroCMSDate
+
+export type Owner = {
+	id: string;
+	name: string;
+}& MicroCMSDate
+
+export type Book = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+  image: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  categories: Category[];
+  owner: Category;
+};
+
+export type BookResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Book[];
+};
+
+export const getBooks = async (queries?: MicroCMSQueries) => {
+  return await client.get<BookResponse>({ endpoint: "books", queries });
+};
+
+export const getBookDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Book>({
+    endpoint: "books",
+    contentId,
+    queries,
+  });
+};
